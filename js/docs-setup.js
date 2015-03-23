bf2718dc08ce11c7882054440ff12f7204278189
J. Harshbarger <hypercubed@gmail.com>
2015-03-23 23:00:02 +0900

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,88 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "An entity-component-system game framework made specifically for AngularJS.",
+      "keywords": "angular angular-ecs angularjs api architecture asset based built canvas cooperate dependency develop developing engine engines entity-component-system feel framework game games great include injection javascript leave loop management native needed nice optimized overview parts pieces pixel play playing rendering specialized tricks web"
+    },
+    {
+      "section": "api",
+      "id": "hc.ngEcs.$components",
+      "shortName": "hc.ngEcs.$components",
+      "type": "service",
+      "moduleName": "hc.ngEcs",
+      "shortDescription": "Index of components",
+      "keywords": "$components api components hc ngecs service"
+    },
+    {
+      "section": "api",
+      "id": "hc.ngEcs.$componentsProvider",
+      "shortName": "hc.ngEcs.$componentsProvider",
+      "type": "service",
+      "moduleName": "hc.ngEcs",
+      "shortDescription": "This provider allows component registration via the register method.",
+      "keywords": "$componentsprovider $register allows annotations api array component components componnet configuration constructors decorated di fn hc map method names ngecs notation object phase provider register registers registration service values"
+    },
+    {
+      "section": "api",
+      "id": "hc.ngEcs.$entities",
+      "shortName": "hc.ngEcs.$entities",
+      "type": "service",
+      "moduleName": "hc.ngEcs",
+      "shortDescription": "Index of entities",
+      "keywords": "$entities api entities hc ngecs service"
+    },
+    {
+      "section": "api",
+      "id": "hc.ngEcs.$systems",
+      "shortName": "hc.ngEcs.$systems",
+      "type": "service",
+      "moduleName": "hc.ngEcs",
+      "shortDescription": "Index of systems",
+      "keywords": "$systems api hc ngecs service systems"
+    },
+    {
+      "section": "api",
+      "id": "hc.ngEcs.$systemsProvider",
+      "shortName": "hc.ngEcs.$systemsProvider",
+      "type": "service",
+      "moduleName": "hc.ngEcs",
+      "shortDescription": "This provider allows component registration via the register method.",
+      "keywords": "$register $systemsprovider allows annotations api array component componnet configuration constructors decorated di fn hc map method names ngecs notation object phase provider register registers registration service system systems values"
+    },
+    {
+      "section": "api",
+      "id": "hc.ngEcs.Entity",
+      "shortName": "hc.ngEcs.Entity",
+      "type": "service",
+      "moduleName": "hc.ngEcs",
+      "shortDescription": "An Entity is bag of game properties (components).  By convention properties that do not start with a $ or _ are considered compoenets.",
+      "keywords": "$add $emit $on $remove adds api args arguments bag call calling compoenets compoent component configuration considered convention deregistration dispatches emit emitted entity event function game hc instance key listen listener listeners ngecs notifying optional passed position properties registered removes returns service start upgrade"
+    },
+    {
+      "section": "api",
+      "id": "hc.ngEcs.ngEcs",
+      "shortName": "hc.ngEcs.ngEcs",
+      "type": "service",
+      "moduleName": "hc.ngEcs",
+      "shortDescription": "ECS engine. Contain System, Components, and Entities.",
+      "keywords": "$c $e $s $start $stop $update adds api array calls collision component components config configuration control contructor creates cycle ecs engine entities entity game hc instance key loop ngecs object player position prototype service starts stops system update"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
