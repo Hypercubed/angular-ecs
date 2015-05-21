41c2ffa9d0929fcfc54ae500926210b0ffa1c0c3
J. Harshbarger <hypercubed@gmail.com>
2015-05-21 21:36:26 +0900

    Version number

--- dist/angular-ecs.min.js
+++ dist/angular-ecs.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-ecs - An ECS framework built for AngularJS
- * @version v0.0.13
+ * @version v0.0.14
  * @link https://github.com/Hypercubed/angular-ecs
  * @author Jayson Harshbarger <>
  * @license 
